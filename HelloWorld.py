013b8d5930a545c1faf019b92c6c66356fa3c8f3
Supreeth Varadarajan <74008521+SupreethVRajan@users.noreply.github.com>
2021-11-25 20:53:11 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,6 @@
-def multiply(a, b):
-  return a*b
+import math
+def pythagoras_hyp(a, b):
+  return (math.sqrt(a*a + b*b))
 
 if __name__ == "__main__":
-  print(multiply(5, 5))
+  print(pythagoras_hyp(3, 4))
