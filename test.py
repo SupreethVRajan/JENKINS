175bf40545b2cea5da7795b344ff630f4d9dda54
Supreeth Varadarajan <74008521+SupreethVRajan@users.noreply.github.com>
2021-11-25 20:53:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import HelloWorld
 
 def test():
 
-	assert HelloWorld.multiply(5, 10) == 50
+	assert HelloWorld.pythagoras_hyp(5, 12) - 13.0 < 0.001
 	
-	assert HelloWorld.multiply(5, -10) == -50
+	assert HelloWorld.pythagoras_hyp(6, 8) - 10 < 0.001
 
 if __name__ == "__main__":
 	test()
